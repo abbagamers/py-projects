239295134b97cb07a0c421986c433cacd3301b01
abbagamers <78752279+abbagamers@users.noreply.github.com>
2022-05-30 13:55:51 +0100

    relpaced values with john age and name

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 print ("hello world")
 char_name = "John"
 char_age = "35"
-print("there was a man named "+ char_name + "he was 35")
+print("there was a man named "+ char_name + "he was " + char_age + ".")
