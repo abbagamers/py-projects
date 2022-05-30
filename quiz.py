b97cf667f4ebea22d6869c848eb2fa2010fb20ae
abbagamers <78752279+abbagamers@users.noreply.github.com>
2022-05-30 15:12:10 +0100

    added 2 questions for the quiz, works well :) Has amount of points there.

--- quiz.py
+++ quiz.py
@@ -1,10 +1,27 @@
 import sys
 import time
-def wait_1() -> object:
-    time.sleep(1)
+AnswersRight = 0
 print("Welcome to a test quiz!")
 time.sleep(0.17)
 playing = input("Do you want to play? ")
-wait_1()
 print(playing)
+if playing != "yes":
+    quit()
+print("Ok, lets go :)")
+cpu = input("What does CPU stand for? \n 1) Chad Power Unit \n 2) Central Processing Unit \n 3) Cold Process Uniform \n")
+if cpu == "2":
+    print("Correct!")
+    AnswersRight = AnswersRight+1
+else:
+    print("Incorrect answer!")
+
+ram = input("What does RAM stand for?  \n 1) Random Ass Man \n 2) Right All Memory \n 3) Random Access Memory \n")
+if ram == "3":
+    print("Correct!")
+    AnswersRight = AnswersRight+1
+else:
+    print("Incorrect. Better Luck!")#
+
+print("You got " + AnswersRight + "answers right!")
+quit()
 
---
+++ quizdefs.py
@@ -0,0 +1,2 @@
+from quiz import *
+cpu = input("What does CPU stand for? \n 1) Chad Power Unit \n 2) Central Processing Unit \n 3) Cold Process Uniform")
