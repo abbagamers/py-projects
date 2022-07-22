19097aa8f855d88d470b2a5ea6a1ce61c264dfb6
abbagamers <malik.g.abba@gmail.com>
2022-07-22 12:44:14 +0100

    used eval for efficiency

--- calc.py
+++ calc.py
@@ -3,14 +3,8 @@ while loop:
     in1 = str(input("What is your operator: "))
     in2 = int(input("What is your first number: "))
     in3 = int(input("What is your second number: "))
-    if in1 == "/":
-        print(str(in2) + " / " + str(in3) + " = " + str(in2 / in3))
-    elif in1 == "+":
-        print(str(in2) + " + " + str(in3) + " = " + str(in2 + in3))
-    elif in1 == "*" or "x":
-        print(str(in2) + " x " + str(in3) + " = " + str(in2 * in3))
-    elif in1 == "-":
-        print(str(in2) + " - " + str(in3) + " = " + str(in2 - in3))
+    if in1 in ['/', '*', '+', '-']:
+        print(f"{in2} {in1} {in3} = {eval(f'{in2}{in1}{in3}')}")
     else:
         print("Invalid")
     in4 = input("Would you like to continue? (y/n) ")
