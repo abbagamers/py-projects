7667c225546d76fd6f6ea31fa0a1382191e9aac2
abbagamers <malik.g.abba@gmail.com>
2022-10-21 11:23:16 +0100

    commented the arduino code

--- automation.py
+++ automation.py
@@ -2,22 +2,22 @@ import time, os, requests
 from apifile import *
 api_key = api
 import serial
-ports = serial.tools.list_ports.comports()
-serialInst = serial.Serial()
-portsList = []
-for oneport in ports:
-    portsList.append(str(oneport))
-    print(str(oneport))
+#ports = serial.tools.list_ports.comports()
+#serialInst = serial.Serial()
+#portsList = []
+#for oneport in ports:
+#    portsList.append(str(oneport))
+#    print(str(oneport))
 
 
-val = input('Select COM Port:')
-for x in range(0,len(portsList)):
-    if portsList[x].startswith('COM' + str(val)):
-        portsVar = 'COM' + str(val)
-        print(portsVar)
-serialInst.baudrate = 9600
-serialInst.port = portsVar
-serialInst.open()
+#val = input('Select COM Port:')
+#for x in range(0,len(portsList)):
+#    if portsList[x].startswith('COM' + str(val)):
+#        portsVar = 'COM' + str(val)
+#        print(portsVar)
+#serialInst.baudrate = 9600
+#serialInst.port = portsVar
+#serialInst.open()
 #api stored in another file
 city_input = input("What city would you like to know the weather forecast for: ")
 weather_data = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={city_input}&units=metric&APPID={api_key}')
@@ -26,7 +26,6 @@ more_lines = [city_input, cod]
 if cod != 200:
     with open('errors\log.txt', 'a') as f:
      f.write('\n'.join(more_lines))
-     serialInst
 weather = weather_data.json()['weather'][0]['main']
 temp = round(weather_data.json()['main']['temp'])
 feels_like = round(weather_data.json()['main']['feels_like'])
