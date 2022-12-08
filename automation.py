dde8a9bbfcada86878a962698d588f24d512cf8b
abbagamers <malik.g.abba@gmail.com>
2022-12-08 16:45:26 +0000

    udemy

--- automation.py
+++ automation.py
@@ -2,23 +2,7 @@ import time, os, requests
 from apifile import *
 api_key = api
 import serial
-#ports = serial.tools.list_ports.comports()
-#serialInst = serial.Serial()
-#portsList = []
-#for oneport in ports:
-#    portsList.append(str(oneport))
-#    print(str(oneport))
 
-
-#val = input('Select COM Port:')
-#for x in range(0,len(portsList)):
-#    if portsList[x].startswith('COM' + str(val)):
-#        portsVar = 'COM' + str(val)
-#        print(portsVar)
-#serialInst.baudrate = 9600
-#serialInst.port = portsVar
-#serialInst.open()
-#api stored in another file
 city_input = input("What city would you like to know the weather forecast for: ")
 weather_data = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={city_input}&units=metric&APPID={api_key}')
 cod = weather_data.json()['cod']
