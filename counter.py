b9a208c80c965d9cdc23e52d027d05fb633d665b
abbagamers <malik.g.abba@gmail.com>
2022-12-08 16:45:41 +0000

    udemy stuff

--- counter.py
+++ counter.py
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
 import time
 x  = 1
 while x >= -1:
     print(x)
     x = x + 1
-=======
-num  = 1
-while num <= 100:
-    print(num)
-    num = num + 1
->>>>>>> bb5e8a87577377c71cdbf55f20b1e764d484f5d4
