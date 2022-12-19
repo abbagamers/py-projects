857d7d5b114f7e7cee95916364eb4f998aece035
abbagamers <malik.g.abba@gmail.com>
2022-12-19 14:05:18 +0000

    dictionary work on pprint methods, basic char finding program

--- dictionaries.py
+++ dictionaries.py
@@ -1,5 +1,14 @@
+import pprint
 car = {'name': 'corsa', 'colour': 'red'}
 if  'corsa' in car.values():
     print('true')
 elif 'corsa' not in car.values():
     print('false')
+
+message = str(input('What text would you like to see the characters for? '))
+count = {}
+for character in message.upper():
+    count.setdefault(character, 0)
+    count[character] = count[character] + 1
+pprint.pprint(count)
+print(count)
